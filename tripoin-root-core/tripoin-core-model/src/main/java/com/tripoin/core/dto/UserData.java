4a7cc098b6e17b3b84d3beda15baa561eb8820c6
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2015-06-29 15:44:00 +0700

    Change UserDetails from spring security core

---
+++ tripoin-root-core/tripoin-core-model/src/main/java/com/tripoin/core/dto/UserData.java
@@ -0,0 +1,128 @@
+package com.tripoin.core.dto;
+
+import com.tripoin.core.pojo.User;
+import com.tripoin.core.util.ParameterConstant;
+
+/**
+ * @author <a href="mailto:ridla.fadilah@gmail.com">Ridla Fadilah</a>
+ */
+public class UserData {
+
+	private Integer id;
+	private String username;
+	private String password;
+	private Integer enabled;
+	private String expiredDate;
+	private Integer nonLocked;
+	private String auth;
+	private Integer status;
+	private String remarks;
+	private RoleData roleData;
+	
+	public UserData(User user) {
+		if(user != null){
+			this.setId(user.getId());
+			this.setUsername(user.getUsername());
+			this.setPassword(user.getPassword());
+			this.setEnabled(user.getEnabled());
+			if(user.getExpiredDate() != null)
+				this.setExpiredDate(ParameterConstant.FORMAT_DEFAULT.format(user.getExpiredDate()));
+			this.setNonLocked(user.getNonLocked());
+			this.setAuth(user.getAuth());
+			this.setStatus(user.getStatus());
+			this.setRemarks(user.getRemarks());
+			if(user.getRole() != null)
+				this.setRoleData(new RoleData(user.getRole()));
+		}
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Integer enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getExpiredDate() {
+		return expiredDate;
+	}
+
+	public void setExpiredDate(String expiredDate) {
+		this.expiredDate = expiredDate;
+	}
+
+	public Integer getNonLocked() {
+		return nonLocked;
+	}
+
+	public void setNonLocked(Integer nonLocked) {
+		this.nonLocked = nonLocked;
+	}
+
+	public String getAuth() {
+		return auth;
+	}
+
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+
+	public RoleData getRoleData() {
+		return roleData;
+	}
+
+	public void setRoleData(RoleData roleData) {
+		this.roleData = roleData;
+	}
+
+	@Override
+	public String toString() {
+		return "UserData [id=" + id + ", username=" + username + ", password="
+				+ password + ", enabled=" + enabled + ", expiredDate="
+				+ expiredDate + ", nonLocked=" + nonLocked + ", auth=" + auth
+				+ ", status=" + status + ", remarks=" + remarks + ", roleData="
+				+ roleData + "]";
+	}
+
+}
