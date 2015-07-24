d7129239b2e6fb9fad8b0ebb14cb658bb340a623
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2015-07-25 06:27:21 +0700

    Role Anonymous Constant

--- tripoin-root-core/tripoin-core-model/src/main/java/com/tripoin/core/common/RoleConstant.java
+++ tripoin-root-core/tripoin-core-model/src/main/java/com/tripoin/core/common/RoleConstant.java
@@ -5,6 +5,7 @@ package com.tripoin.core.common;
  */
 public class RoleConstant {
 
+	public static final String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
 	public static final String ROLE_SUPERADMIN = "ROLE_SUPERADMIN";
 	public static final String ROLE_ADMIN = "ROLE_ADMIN";
 	public static final String ROLE_USER = "ROLE_USER";
