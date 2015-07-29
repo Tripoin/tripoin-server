49471a3aa983ed0d640a2c7c6989326181ca8b71
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2015-07-29 17:57:50 +0700

    Add gwt.xml and move authentication handler to IAccessControl

--- tripoin-root-web/tripoin-web-ui/src/main/java/com/tripoin/web/authentication/impl/AccessControlImpl.java
+++ tripoin-root-web/tripoin-web-ui/src/main/java/com/tripoin/web/authentication/impl/AccessControlImpl.java
@@ -1,12 +1,17 @@
 package com.tripoin.web.authentication.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.tripoin.core.common.RoleConstant;
-import com.tripoin.web.authentication.BaseAuthentication;
 import com.tripoin.web.authentication.IAccessControl;
 
 /**
@@ -14,35 +19,41 @@ import com.tripoin.web.authentication.IAccessControl;
  */
 public class AccessControlImpl implements IAccessControl {
 
-    private BaseAuthentication baseAuthentication;    
-	
-	public void setBaseAuthentication(BaseAuthentication baseAuthentication) {
-		this.baseAuthentication = baseAuthentication;
-	}
-
-	@Secured({RoleConstant.ROLE_SUPERADMIN, RoleConstant.ROLE_ADMIN, RoleConstant.ROLE_USER})
-	@Override
-	public void clearSecurityContext(){
-		baseAuthentication.logout();		
-	}
+    private static Logger LOGGER = LoggerFactory.getLogger(AccessControlImpl.class);
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
 	@Override
     public boolean signIn(String username, String password) {
-		boolean sucess = baseAuthentication.login(username, password);
-        return sucess;
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            if (authentication.isAuthenticated()){
+            	LOGGER.debug("User [{}] successfully logged in application", username);
+            	return true;            	
+            }
+        } catch (BadCredentialsException bce) {
+        	SecurityContextHolder.clearContext();
+        	LOGGER.warn("Cannot login : {}", bce.getMessage());
+        } catch (Exception e) {
+        	SecurityContextHolder.clearContext();
+        	LOGGER.warn("Authentication failed for user [{}]", username);
+        }
+        return false;
     }
 
     @Override
     public boolean isUserSignedIn() {
-    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();	
-        if (authentication != null){
-        	GrantedAuthority grantedAuthority = authentication.getAuthorities().iterator().next();
-        	if(!RoleConstant.ROLE_ANONYMOUS.equals(grantedAuthority.getAuthority()))
-        		return true;
-        	else
-        		return false;
-        }else
-        	return false;
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getAuthorities().size() > 0){
+        	for(GrantedAuthority grantedAuthority : authentication.getAuthorities()){
+        		if(!RoleConstant.ROLE_ANONYMOUS.equals(grantedAuthority.getAuthority()))
+        			return true;
+        	}
+        }
+        return false;
     }
 
     @Secured({RoleConstant.ROLE_SUPERADMIN, RoleConstant.ROLE_ADMIN, RoleConstant.ROLE_USER})
@@ -57,7 +68,7 @@ public class AccessControlImpl implements IAccessControl {
 	@Secured({RoleConstant.ROLE_SUPERADMIN, RoleConstant.ROLE_ADMIN, RoleConstant.ROLE_USER})
 	@Override
 	public String getRole() {	
-    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     	if(authentication == null)
     		return null;
     	GrantedAuthority grantedAuthority = authentication.getAuthorities().iterator().next();
