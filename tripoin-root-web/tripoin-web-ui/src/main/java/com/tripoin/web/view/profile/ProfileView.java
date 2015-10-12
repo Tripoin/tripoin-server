6e45675e781071ec77d0d4822036cb8fe6bf4d78
Ginanjar Aji Sanjaya <ganjar.snjy@gmail.com>
2015-10-12 10:32:06 +0700

    view profile

--- tripoin-root-web/tripoin-web-ui/src/main/java/com/tripoin/web/view/profile/ProfileView.java
+++ tripoin-root-web/tripoin-web-ui/src/main/java/com/tripoin/web/view/profile/ProfileView.java
@@ -1,5 +1,6 @@
 package com.tripoin.web.view.profile;
 
+import java.io.File;
 import java.util.Date;
 
 import javax.annotation.PostConstruct;
@@ -12,10 +13,9 @@ import com.tripoin.core.common.ParameterConstant;
 import com.tripoin.core.dto.ProfileData;
 import com.tripoin.web.service.IProfileService;
 import com.tripoin.web.servlet.VaadinView;
-import com.tripoin.web.view.page.StringGenerator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.server.UserError;
+import com.vaadin.server.FileResource;
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
@@ -26,10 +26,10 @@ import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.OptionGroup;
 import com.vaadin.ui.RichTextArea;
-import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
@@ -55,37 +55,60 @@ public class ProfileView extends VerticalLayout implements View {
         setSpacing(true);
         setMargin(true);
 
+        final FormLayout formTitle = new FormLayout();
+        formTitle.setMargin(false);
+        
         Label title = new Label("Edit Profile");
         title.addStyleName("h1");
-        addComponent(title);
+        formTitle.addComponent(title);
+        
+        Label section = new Label("Personal Info");
+        section.addStyleName("h3");
+        section.addStyleName("colored");
+        formTitle.addComponent(section);
+        formTitle.setComponentAlignment(section, Alignment.MIDDLE_LEFT);
+
+        HorizontalLayout row = new HorizontalLayout();
+        row.setSpacing(true);
+        row.setMargin(false);
+        row.setWidth("800px");
+        row.addStyleName("light");
+        row.addComponent(formTitle);
+        addComponent(row);
+        
+        Image profilePhoto = new Image();
+        profilePhoto.setSource(new FileResource(new File(profileData.getPhoto())));
+        profilePhoto.setWidth("150px");
+        profilePhoto.setHeight("150px");
+        row.addComponent(profilePhoto);
+        row.setComponentAlignment(profilePhoto, Alignment.TOP_RIGHT);
 
         final FormLayout form = new FormLayout();
         form.setMargin(false);
         form.setWidth("800px");
         form.addStyleName("light");
         addComponent(form);
-
-        Label section = new Label("Personal Info");
-        section.addStyleName("h2");
-        section.addStyleName("colored");
-        form.addComponent(section);
-        StringGenerator sg = new StringGenerator();
-
-        TextField name = new TextField("Name");
+        
+        final TextField name = new TextField("Name");
         name.setValue(profileData.getName());
         name.setWidth("50%");
         form.addComponent(name);
 
-        DateField birthday = new DateField("Place of Birth");
+        final DateField birthday = new DateField("Birth");
         birthday.setValue(new Date(80, 0, 31));
         form.addComponent(birthday);
 
-        TextField username = new TextField("Username");
-        username.setValue(sg.nextString(false) + sg.nextString(false));
-        username.setRequired(true);
+        final TextField username = new TextField("Username");
+        username.setValue(profileData.getUserData().getUsername());
+        username.setWidth("50%");
         form.addComponent(username);
+        
+        final TextField password = new TextField("Password");
+        password.setValue(profileData.getUserData().getPassword());
+        password.setWidth("50%");
+        form.addComponent(password);
 
-        OptionGroup sex = new OptionGroup("Sex");
+        final OptionGroup sex = new OptionGroup("Gender");
         sex.addItem(ParameterConstant.FEMALE);
         sex.addItem(ParameterConstant.MALE);
         if(ParameterConstant.MALE.equalsIgnoreCase(profileData.getSex()))
@@ -100,31 +123,30 @@ public class ProfileView extends VerticalLayout implements View {
         section.addStyleName("colored");
         form.addComponent(section);
 
-        TextField email = new TextField("Email");
+        final TextField address = new TextField("Address");
+        address.setValue(profileData.getAddress());
+        address.setWidth("50%");
+        form.addComponent(address);
+
+        final TextField phone = new TextField("Phone");
+        phone.setWidth("50%");
+        phone.setValue(profileData.getPhone());
+        form.addComponent(phone);
+
+        final TextField email = new TextField("Email");
         email.setValue(profileData.getEmail());
         email.setWidth("50%");
         email.setRequired(true);
         form.addComponent(email);
 
-        TextField location = new TextField("Location");
-        location.setValue(sg.nextString(true) + ", " + sg.nextString(true));
-        location.setWidth("50%");
-        location.setComponentError(new UserError("This address doesn't exist"));
-        form.addComponent(location);
-
-        TextField phone = new TextField("Phone");
-        phone.setWidth("50%");
-        phone.setValue(profileData.getPhone());
-        form.addComponent(phone);
-
         HorizontalLayout wrap = new HorizontalLayout();
         wrap.setSpacing(true);
         wrap.setDefaultComponentAlignment(Alignment.MIDDLE_LEFT);
         wrap.setCaption("Newsletter");
-        CheckBox newsletter = new CheckBox("Subscribe to newsletter", true);
+        final CheckBox newsletter = new CheckBox("Subscribe to newsletter", true);
         wrap.addComponent(newsletter);
 
-        ComboBox period = new ComboBox();
+        final ComboBox period = new ComboBox();
         period.setTextInputAllowed(false);
         period.addItem("Daily");
         period.addItem("Weekly");
@@ -141,23 +163,22 @@ public class ProfileView extends VerticalLayout implements View {
         section.addStyleName("colored");
         form.addComponent(section);
 
-        TextField website = new TextField("Website");
-        website.setInputPrompt("http://");
-        website.setWidth("100%");
-        form.addComponent(website);
-
-        TextArea shortbio = new TextArea("Short Bio");
-        shortbio.setValue("Quis aute iure reprehenderit in voluptate velit esse. Cras mattis iudicium purus sit amet fermentum.");
-        shortbio.setWidth("100%");
-        shortbio.setRows(2);
-        form.addComponent(shortbio);
-
         final RichTextArea bio = new RichTextArea("Bio");
         bio.setWidth("100%");
         bio.setValue("<div><p><span>Integer legentibus erat a ante historiarum dapibus.</span> <span>Vivamus sagittis lacus vel augue laoreet rutrum faucibus.</span> <span>A communi observantia non est recedendum.</span> <span>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</span> <span>Ab illo tempore, ab est sed immemorabili.</span> <span>Quam temere in vitiis, legem sancimus haerentia.</span></p><p><span>Morbi odio eros, volutpat ut pharetra vitae, lobortis sed nibh.</span> <span>Quam diu etiam furor iste tuus nos eludet?</span> <span>Cum sociis natoque penatibus et magnis dis parturient.</span> <span>Quam diu etiam furor iste tuus nos eludet?</span> <span>Tityre, tu patulae recubans sub tegmine fagi  dolor.</span></p><p><span>Curabitur blandit tempus ardua ridiculus sed magna.</span> <span>Phasellus laoreet lorem vel dolor tempus vehicula.</span> <span>Etiam habebis sem dicantur magna mollis euismod.</span> <span>Hi omnes lingua, institutis, legibus inter se differunt.</span></p></div>");
         form.addComponent(bio);
 
         form.setReadOnly(true);
+        name.setReadOnly(true);
+        birthday.setReadOnly(true);
+        username.setReadOnly(true);
+        password.setReadOnly(true);
+        sex.setReadOnly(true);
+        address.setReadOnly(true);
+        email.setReadOnly(true);
+        newsletter.setReadOnly(true);
+        phone.setReadOnly(true);
+        period.setReadOnly(true);
         bio.setReadOnly(true);
 
         Button edit = new Button("Edit", new ClickListener() {
@@ -166,14 +187,36 @@ public class ProfileView extends VerticalLayout implements View {
             public void buttonClick(ClickEvent event) {
                 boolean readOnly = form.isReadOnly();
                 if (readOnly) {
-                    bio.setReadOnly(false);
                     form.setReadOnly(false);
+                    name.setReadOnly(false);
+                    birthday.setReadOnly(false);
+                    username.setReadOnly(false);
+                    password.setReadOnly(false);
+                    sex.setReadOnly(false);
+                    address.setReadOnly(false);
+                    email.setReadOnly(false);
+                    newsletter.setReadOnly(false);
+                    phone.setReadOnly(false);
+                    period.setReadOnly(false);
+                    bio.setReadOnly(false);
+                    
                     form.removeStyleName("light");
                     event.getButton().setCaption("Save");
                     event.getButton().addStyleName("primary");
                 } else {
-                    bio.setReadOnly(true);
                     form.setReadOnly(true);
+                    name.setReadOnly(true);
+                    birthday.setReadOnly(true);
+                    username.setReadOnly(true);
+                    password.setReadOnly(true);
+                    sex.setReadOnly(true);
+                    address.setReadOnly(true);
+                    email.setReadOnly(true);
+                    newsletter.setReadOnly(true);
+                    phone.setReadOnly(true);
+                    period.setReadOnly(true);
+                    bio.setReadOnly(true);
+                    
                     form.addStyleName("light");
                     event.getButton().setCaption("Edit");
                     event.getButton().removeStyleName("primary");
