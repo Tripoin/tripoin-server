bad5707a8d45e851531a48bdb6ad7140ec0d52fe
Ginanjar Aji Sanjaya <ganjar.snjy@gmail.com>
2015-10-15 04:54:39 +0700

    ws update

--- tripoin-root-web/tripoin-web-service/src/main/java/com/tripoin/web/common/WebServiceConstant.java
+++ tripoin-root-web/tripoin-web-service/src/main/java/com/tripoin/web/common/WebServiceConstant.java
@@ -7,7 +7,7 @@ public class WebServiceConstant {
 
 	private static final String HTTP_LOAD = "/load";
 	private static final String HTTP_SAVE = "/save";
-	/*private static final String HTTP_UPDATE = "/update";*/
+	private static final String HTTP_UPDATE = "/update";
 	/*private static final String HTTP_DELETE = "/delete";*/
 	
 	public static final String HTTP_ONLY = "HttpOnly";
@@ -16,11 +16,13 @@ public class WebServiceConstant {
 	public static final String HTTP_LOGIN = "/login";	
 	public static final String HTTP_LOGIN_MENU = "/login-menu";
 	public static final String HTTP_PROFILE = "/profile".concat(HTTP_LOAD);
+	public static final String HTTP_PROFILE_SAVE = "/profile".concat(HTTP_SAVE);
 	public static final String HTTP_AVAILABILITY = "/availability".concat(HTTP_LOAD);
 	public static final String HTTP_CATEGORY = "/category".concat(HTTP_LOAD);
 	public static final String HTTP_PRODUCT = "/product".concat(HTTP_LOAD);
 	public static final String HTTP_USER = "/user".concat(HTTP_LOAD);
 	public static final String HTTP_USER_SAVE = "/user".concat(HTTP_SAVE);
+	public static final String HTTP_USER_UPDATE = "/user".concat(HTTP_UPDATE);
 	public static final String HTTP_PRODUCT_SAVE = "/product".concat(HTTP_SAVE);
 
 }
