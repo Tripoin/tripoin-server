dce0537b7eba52a224fca7beb7a698d15ade9950
Ginanjar Aji Sanjaya <ganjar.snjy@gmail.com>
2015-10-15 05:05:56 +0700

    Insert, update n view change password

--- tripoin-root-web/tripoin-web-ui/src/main/java/com/tripoin/web/view/profile/ProfileView.java
+++ tripoin-root-web/tripoin-web-ui/src/main/java/com/tripoin/web/view/profile/ProfileView.java
@@ -9,21 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.gargoylesoftware.htmlunit.WebConsole.Logger;
 import com.tripoin.core.common.ParameterConstant;
 import com.tripoin.core.dto.ProfileData;
 import com.tripoin.web.service.IProfileService;
 import com.tripoin.web.servlet.VaadinView;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.server.ExternalResource;
 import com.vaadin.server.FileResource;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.CheckBox;
-import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
@@ -67,14 +66,14 @@ public class ProfileView extends VerticalLayout implements View {
         row.setSpacing(true);
         row.setMargin(false);
         row.setWidth("800px");
-        row.addStyleName("light");
         row.addComponent(formTitle);
         addComponent(row);
         
         Image profilePhoto = new Image();
-        profilePhoto.setSource(new File(profileData.getPhoto()).exists() ? new FileResource(new File(profileData.getPhoto())) : new ExternalResource("http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"));
+        profilePhoto.setSource(new File(profileData.getPhoto()).exists() ? new FileResource(new File(profileData.getPhoto())) : new ThemeResource("../tripoin-valo/img/profile-pic-new-300px.png")); 
         profilePhoto.setWidth("150px");
         profilePhoto.setHeight("150px");
+        profilePhoto.addStyleName("light2");
         row.addComponent(profilePhoto);
         row.setComponentAlignment(profilePhoto, Alignment.TOP_RIGHT);
 
@@ -103,11 +102,6 @@ public class ProfileView extends VerticalLayout implements View {
         username.setValue(profileData.getUserData().getUsername());
         username.setWidth("50%");
         form.addComponent(username);
-        
-        final TextField password = new TextField("Password");
-        password.setValue(profileData.getUserData().getPassword());
-        password.setWidth("50%");
-        form.addComponent(password);
 
         final OptionGroup sex = new OptionGroup("Gender");
         sex.addItem(ParameterConstant.FEMALE);
@@ -140,46 +134,24 @@ public class ProfileView extends VerticalLayout implements View {
         email.setRequired(true);
         form.addComponent(email);
 
-        HorizontalLayout wrap = new HorizontalLayout();
-        wrap.setSpacing(true);
-        wrap.setDefaultComponentAlignment(Alignment.MIDDLE_LEFT);
-        wrap.setCaption("Newsletter");
-        final CheckBox newsletter = new CheckBox("Subscribe to newsletter", true);
-        wrap.addComponent(newsletter);
-
-        final ComboBox period = new ComboBox();
-        period.setTextInputAllowed(false);
-        period.addItem("Daily");
-        period.addItem("Weekly");
-        period.addItem("Monthly");
-        period.setNullSelectionAllowed(false);
-        period.select("Weekly");
-        period.addStyleName("small");
-        period.setWidth("10em");
-        wrap.addComponent(period);
-        form.addComponent(wrap);
-
         section = new Label("Additional Info");
         section.addStyleName("h4");
         section.addStyleName("colored");
         form.addComponent(section);
 
         final RichTextArea bio = new RichTextArea("Bio");
+        bio.setValue(profileData.getBio());
         bio.setWidth("100%");
-        bio.setValue("<div><p><span>Integer legentibus erat a ante historiarum dapibus.</span> <span>Vivamus sagittis lacus vel augue laoreet rutrum faucibus.</span> <span>A communi observantia non est recedendum.</span> <span>Morbi fringilla convallis sapien, id pulvinar odio volutpat.</span> <span>Ab illo tempore, ab est sed immemorabili.</span> <span>Quam temere in vitiis, legem sancimus haerentia.</span></p><p><span>Morbi odio eros, volutpat ut pharetra vitae, lobortis sed nibh.</span> <span>Quam diu etiam furor iste tuus nos eludet?</span> <span>Cum sociis natoque penatibus et magnis dis parturient.</span> <span>Quam diu etiam furor iste tuus nos eludet?</span> <span>Tityre, tu patulae recubans sub tegmine fagi  dolor.</span></p><p><span>Curabitur blandit tempus ardua ridiculus sed magna.</span> <span>Phasellus laoreet lorem vel dolor tempus vehicula.</span> <span>Etiam habebis sem dicantur magna mollis euismod.</span> <span>Hi omnes lingua, institutis, legibus inter se differunt.</span></p></div>");
         form.addComponent(bio);
 
         form.setReadOnly(true);
         name.setReadOnly(true);
         birthday.setReadOnly(true);
         username.setReadOnly(true);
-        password.setReadOnly(true);
         sex.setReadOnly(true);
         address.setReadOnly(true);
         email.setReadOnly(true);
-        newsletter.setReadOnly(true);
         phone.setReadOnly(true);
-        period.setReadOnly(true);
         bio.setReadOnly(true);
 
         Button edit = new Button("Edit", new ClickListener() {
@@ -192,30 +164,33 @@ public class ProfileView extends VerticalLayout implements View {
                     name.setReadOnly(false);
                     birthday.setReadOnly(false);
                     username.setReadOnly(false);
-                    password.setReadOnly(false);
                     sex.setReadOnly(false);
                     address.setReadOnly(false);
                     email.setReadOnly(false);
-                    newsletter.setReadOnly(false);
                     phone.setReadOnly(false);
-                    period.setReadOnly(false);
                     bio.setReadOnly(false);
                     
                     form.removeStyleName("light");
                     event.getButton().setCaption("Save");
                     event.getButton().addStyleName("primary");
                 } else {
+                	profileData.setName(name.getValue());
+                	profileData.setBirthdate("20150909");
+                	profileData.getUserData().setUsername(username.getValue());
+                	profileData.setSex(sex.getValue().toString());
+                	profileData.setAddress(address.getValue());
+                	profileData.setPhone(phone.getValue());
+                	profileData.setEmail(email.getValue());
+                	profileData.setBio(bio.getValue());
+                	String as = profileService.updateProfile(profileData, profileData.getUserData());
                     form.setReadOnly(true);
                     name.setReadOnly(true);
                     birthday.setReadOnly(true);
                     username.setReadOnly(true);
-                    password.setReadOnly(true);
                     sex.setReadOnly(true);
                     address.setReadOnly(true);
                     email.setReadOnly(true);
-                    newsletter.setReadOnly(true);
                     phone.setReadOnly(true);
-                    period.setReadOnly(true);
                     bio.setReadOnly(true);
                     
                     form.addStyleName("light");
