82ff115ac72d5300f31d99f742ea87a71eda87cd
Ginanjar Aji Sanjaya <ganjar.snjy@gmail.com>
2015-10-15 05:04:51 +0700

    add menu change password

--- tripoin-root-web/tripoin-web-ui/src/main/java/com/tripoin/web/view/menu/BaseMenuLayout.java
+++ tripoin-root-web/tripoin-web-ui/src/main/java/com/tripoin/web/view/menu/BaseMenuLayout.java
@@ -92,17 +92,23 @@ public class BaseMenuLayout extends CssLayout implements View {
 	private Component buildUserMenu() {
         final MenuBar settings = new MenuBar();
         settings.addStyleName("user-menu");
-        settingsItem = settings.addItem("", new ThemeResource("../tripoin-valo/img/profile-pic-300px.jpg"), null);
-        settingsItem.addItem("Edit Profile", new Command() {
+        settingsItem = settings.addItem("", new ThemeResource("../tripoin-valo/img/profile-pic-new-300px.png"), null);
+        settingsItem.addItem("Account Settings", new Command() {
 			private static final long serialVersionUID = 8813252433421821224L;
 			@Override
 			public void menuSelected(MenuItem selectedItem) {
 				UI.getCurrent().getNavigator().navigateTo("profile");
 			}
 		});
-        settingsItem.addItem("Preferences", null);
+        settingsItem.addItem("Change Password", new Command() {
+			private static final long serialVersionUID = 8813252433421821224L;
+			@Override
+			public void menuSelected(MenuItem selectedItem) {
+				UI.getCurrent().getNavigator().navigateTo("changePassword");
+			}
+		});
         settingsItem.addSeparator();
-        settingsItem.addItem("Sign Out", FontAwesome.SIGN_OUT, new Command() {			
+        settingsItem.addItem("Log Out", FontAwesome.SIGN_OUT, new Command() {			
 			private static final long serialVersionUID = -7829505006330125630L;
 			@Override
 			public void menuSelected(MenuItem selectedItem) {
