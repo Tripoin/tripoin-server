c568d69adb016b28681400842451c302754e4d00
Ginanjar Aji Sanjaya <ganjar.snjy@gmail.com>
2015-10-27 14:57:27 +0700

    PasswordService & impl

---
+++ tripoin-root-web/tripoin-web-service/src/main/java/com/tripoin/web/service/IPasswordService.java
@@ -0,0 +1,12 @@
+package com.tripoin.web.service;
+
+import com.tripoin.core.dto.UserData;
+
+/**
+ * @author <a href="mailto:ridla.fadilah@gmail.com">Ridla Fadilah</a>
+ */
+public interface IPasswordService {
+
+    public String updatePassword(UserData userData);
+
+}
---
+++ tripoin-root-web/tripoin-web-service/src/main/java/com/tripoin/web/service/impl/PasswordServiceImpl.java
@@ -0,0 +1,41 @@
+package com.tripoin.web.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.stereotype.Service;
+
+import com.tripoin.core.dto.UserData;
+import com.tripoin.core.dto.UserTransferObject;
+import com.tripoin.web.common.ICommonRest;
+import com.tripoin.web.common.IStateFullRest;
+import com.tripoin.web.common.WebServiceConstant;
+import com.tripoin.web.service.IPasswordService;
+
+/**
+ * @author <a href="mailto:ridla.fadilah@gmail.com">Ridla Fadilah</a>
+ */
+@Service("passwordService")
+public class PasswordServiceImpl implements IPasswordService {
+
+	private static transient final Logger LOGGER = LoggerFactory.getLogger(PasswordServiceImpl.class);
+
+	@Autowired
+	private ICommonRest commonRest;
+	
+	@Autowired
+	private IStateFullRest stateFullRest;
+
+	@Override
+	public String updatePassword(UserData userData) {
+		stateFullRest.setUsername("ridla");
+		stateFullRest.setPassword("ridla");
+		userData.setPassword(new String(Base64.encode(userData.getPassword().getBytes())));
+		UserTransferObject userTransferObject = stateFullRest.post(commonRest.getUrl(WebServiceConstant.HTTP_PASSWORD_UPDATE), userData, UserTransferObject.class);
+		LOGGER.debug("Response Body User : "+userTransferObject.getUserDatas());
+	
+		return "Successfully";
+	}
+
+}
