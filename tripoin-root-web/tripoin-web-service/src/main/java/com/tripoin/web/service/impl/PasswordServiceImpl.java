37b7cb0e0ee06f944899a0b74ed8e1d225be271e
Ginanjar Aji Sanjaya <ganjar.snjy@gmail.com>
2015-10-28 12:40:08 +0700

    PasswordService & impl

--- tripoin-root-web/tripoin-web-service/src/main/java/com/tripoin/web/service/impl/PasswordServiceImpl.java
+++ tripoin-root-web/tripoin-web-service/src/main/java/com/tripoin/web/service/impl/PasswordServiceImpl.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.codec.Base64;
 import org.springframework.stereotype.Service;
-
 import com.tripoin.core.dto.UserData;
 import com.tripoin.core.dto.UserTransferObject;
 import com.tripoin.web.common.ICommonRest;
@@ -26,12 +25,14 @@ public class PasswordServiceImpl implements IPasswordService {
 	
 	@Autowired
 	private IStateFullRest stateFullRest;
-
+	
 	@Override
 	public String updatePassword(UserData userData) {
-		stateFullRest.setUsername("ridla");
-		stateFullRest.setPassword("ridla");
+		stateFullRest.setUsername(userData.getUsername());
+		stateFullRest.setPassword(userData.getPassword());
+		System.out.println("ANJAR GANTENG ORI = "+userData.getPassword());
 		userData.setPassword(new String(Base64.encode(userData.getPassword().getBytes())));
+		System.out.println("ANJAR GANTENG ENCODE BASE64 = "+userData.getPassword());
 		UserTransferObject userTransferObject = stateFullRest.post(commonRest.getUrl(WebServiceConstant.HTTP_PASSWORD_UPDATE), userData, UserTransferObject.class);
 		LOGGER.debug("Response Body User : "+userTransferObject.getUserDatas());
 	
