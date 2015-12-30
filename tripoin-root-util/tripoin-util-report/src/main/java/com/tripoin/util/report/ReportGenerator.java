35a346a32aee71d8f57f54766d62a6afe6df0b50
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2015-12-30 21:59:46 +0700

    reporting from datasource grid, update commons-beanutils version, jasperreport version, dtd font families

--- tripoin-root-util/tripoin-util-report/src/main/java/com/tripoin/util/report/ReportGenerator.java
+++ tripoin-root-util/tripoin-util-report/src/main/java/com/tripoin/util/report/ReportGenerator.java
@@ -2,11 +2,13 @@ package com.tripoin.util.report;
 
 import java.io.File;
 import java.io.OutputStream;
+import java.sql.Connection;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
@@ -20,11 +22,19 @@ public class ReportGenerator {
 	
 	public void printReportToFile(String reportFilename, JRDataSource dataSource, Map<String, Object> params, OutputStream outputStream) throws Exception {
 		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReportCompiler.getBaseFileReportsPath().getPath().concat(File.separator).concat(reportFilename), params, dataSource);
-        
-        JRPdfExporter exporter = new JRPdfExporter();
+		pdfExporter(jasperPrint, outputStream);
+	}
+	
+	public void printReportToFile(String reportFilename, Connection connection, Map<String, Object> params, OutputStream outputStream) throws Exception {
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReportCompiler.getBaseFileReportsPath().getPath().concat(File.separator).concat(reportFilename), params, connection);
+		pdfExporter(jasperPrint, outputStream);        
+	}
+	
+	private void pdfExporter(JasperPrint jasperPrint, OutputStream outputStream) throws JRException{
+		JRPdfExporter exporter = new JRPdfExporter();
         exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
         exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
         exporter.exportReport();
-	}	
+	}
 
 }
